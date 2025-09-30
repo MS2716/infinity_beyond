e64f00ce16026d655506443096db8f41f446ed07
MS2716 <sutharmaitri@gmail.com>
2025-09-30 16:11:25 +0530

    Vercel deploy

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,18 @@
-import { FlatCompat } from '@eslint/eslintrc'
- 
+import { FlatCompat } from "@eslint/eslintrc";
+
 const compat = new FlatCompat({
   // import.meta.dirname is available after Node.js v20.11.0
   baseDirectory: import.meta.dirname,
-})
- 
+});
+
 const eslintConfig = [
   ...compat.config({
-    extends: ['next/core-web-vitals', 'next/typescript'],
+    extends: "next/core-web-vitals",
+    rules: {
+      // Other rules...
+      "jsx-a11y/anchor-is-valid": "off",
+    },
   }),
-]
- 
-export default eslintConfig
+];
+
+export default eslintConfig;
