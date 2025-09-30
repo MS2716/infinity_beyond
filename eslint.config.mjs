9dfe97a1fafb57155d7f203dbfebada5c8383a2a
MS2716 <sutharmaitri@gmail.com>
2025-09-30 16:07:27 +0530

    Vercel deploy

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -20,6 +20,20 @@ const eslintConfig = [
       "next-env.d.ts",
     ],
   },
+
+   ...compat.config({
+    extends: ['next'],
+    rules: {
+      'react/no-unescaped-entities': 'off',
+      '@next/next/no-page-custom-font': 'off',
+    },
+  }),
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    rules: {
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    },
+  },
 ];
 
 export default eslintConfig;
